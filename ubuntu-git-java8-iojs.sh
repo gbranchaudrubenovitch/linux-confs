df55e3a8da0bb4b5bb25256c1bcb37b684f25f9e
gbranchaudrubenovitch <gabriel.branchaud@gmail.com>
2015-09-14 15:06:28 -0400

    Removed prompts from the script

--- ubuntu-git-java8-iojs.sh
+++ ubuntu-git-java8-iojs.sh
@@ -1,11 +1,16 @@
 # this bootstrapped a ubuntu 14.04 vm supplied by buyvm
 # note: run the entire thing as root
 
-apt-get install software-properties-common curl dialog
+apt-get update
+apt-get --yes upgrade
+apt-get --yes install software-properties-common curl dialog
 
-add-apt-repository ppa:git-core/ppa
-add-apt-repository ppa:webupd8team/java
+add-apt-repository --yes ppa:git-core/ppa
+add-apt-repository --yes ppa:webupd8team/java
 curl -sL https://deb.nodesource.com/setup_iojs_3.x | bash - # does apt-get update for us
 
-apt-get upgrade
-apt-get install git iojs oracle-java8-installer
+# auto accept oracle's license
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+
+apt-get --yes install git iojs oracle-java8-installer
